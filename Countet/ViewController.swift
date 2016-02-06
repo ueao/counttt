faa2b914eb9751cf554a31c539190f2d25ad8881
Sakaue Aoi <aoisakaue@macuser-no-macbook-pro-3.local>
2016-02-06 22:09:47 +0900

    Initial Commit

---
+++ Countet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Countet
+//
+//  Created by Aoi Sakaue on 2016/02/06.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
