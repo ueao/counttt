be2d7aa0342a3cc9db020cf0a233647c48d695b9
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-02-07 15:38:51 +0900

    バースデイプレゼントをカウントするクマです。

--- Countet/ViewController.swift
+++ Countet/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
 
 }
 
